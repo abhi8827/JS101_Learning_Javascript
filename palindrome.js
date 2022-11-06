9e1717af487298f27bd7fd35721684801ab5dfb8
Abhishek Patel <you@example.com>
2022-11-06 06:02:53 +0530

    Created the prime and palindrome files

---
+++ palindrome.js
@@ -0,0 +1,21 @@
+// Guess for the code of Palindrome , is it correct?
+
+ function reverse(N){
+  let bag="";
+  for (let i=N.length-1; i>=0; i--){
+       bag = bag +N[i] ;
+     }
+  console.bag;
+
+}
+
+ let reversed= reverse(N);
+ 
+ if (N===reversed){
+ console.log("Palindrome")}
+
+else {
+ console.log("Not a Palindrome")
+}
+
+reverse(racecar);
---
+++ prime.js
@@ -0,0 +1,24 @@
+// check for the prime code is it correct?
+
+function prime (num){
+ 
+let count=0;
+
+for ( i=1; i<=num; i++){
+
+    if (i % 2 == 0){
+      count++ 
+     }
+  }
+
+if  (count == 2){
+  comsole.log("Yes");
+ }
+
+else 
+  { console.log("No")}
+
+  }
+
+ print(13);
+ print(14);
